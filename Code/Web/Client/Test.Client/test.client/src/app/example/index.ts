e629270d50359a9b173ab8131fffff6d8c49b042
Lieven Baert <SIDLIBA@sidmar.be>
2021-03-05 13:43:15 +0100

    removed obsolete stuff

--- Code/Web/Client/Test.Client/test.client/src/app/example/index.ts
+++
@@ -1,5 +0,0 @@
-/**
- * This is a barrel file. If you want to know  more about barrel files,
- * how to use them and why we use them visit: https://basarat.gitbooks.io/typescript/docs/tips/barrel.html
- */
-export * from './example.component';
